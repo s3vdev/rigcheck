f3685aa022c07105b25b8ab7d861408562ad0356
Sven Mielke <web@ddl.bz>
2018-03-19 12:03:46 +0100

    Added update_rigcheck. Its awesome because you can copy rigcheck.sh to multible ethos machines with only one command.

---
+++ update_rigcheck.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Originally from repository: https://bitbucket.org/defib/rigcheck/src/master/
+# @sixfourtysword
+
+## Usage: put this shell script in the same folder as rigcheck.sh, it will copy that to each ethos machine in ethoservers
+
+##
+# Sven Mielke (march 2018) http://bitbucket.org/s3v3n/rigcheck
+#
+# UPDATE: To prevent passwort input flood, just install "sshpass" (https://gist.github.com/arunoda/7790979), and edit your login details below.
+# @mac OS X: brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
+# @Linux: apt-get update && apt-get install install sshpass
+#
+##
+
+##
+# Insert IP addressess for your Ethos Rigs. CHANGE THESE EXAMPLES
+ethoservers=(
+"192.168.1.1"
+"192.168.1.2"
+"192.168.1.3"
+"192.168.1.4"
+"192.168.1.5"
+"192.168.1.6"
+)
+
+##
+# ethOS Username
+user="ethos";
+
+##
+# ethOS Password
+pass="live";
+
+for sname in "${ethoservers[@]}"
+do
+	##
+	# This one copies the rigcheck script
+	sshpass -p ${pass} scp ./rigcheck.sh ${user}@$sname:/home/ethos/
+
+	##
+	# Uncomment this when you are installing the first time/updating the config file
+	#scp ./rigcheck.config ethos@$sname:/home/ethos/
+
+    ##
+    # This one set chmod 755 to rigcheck.sh
+	sshpass -p${pass} ssh ${user}@$sname chmod a+x /home/ethos/rigcheck.sh
+	##
+	# Sometimes you must run this command (and disable the other one!)
+	#sshpass -p${pass} ssh -o StrictHostKeyChecking=no ${user}@$sname chmod a+x /home/ethos/rigcheck.sh
+
+
+	#TODO add crontab entry if not already there
+done
